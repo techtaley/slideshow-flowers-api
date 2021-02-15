d1fd88f8fc8c66da7c106a3dc14b8f7a3eaf26b0
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-02-15 16:51:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+   const express = require("express")
+   const app = express()
+   const importData = require("./flowers.json")
+   const port = process.env.PORT || 4001
+
+   //routes
+   app.get("/", (req, res) => {
+       res.send("Flower shop")
+  })
+
+   app.get("/flowers", (req, res) => {
+       res.send(importData)
+   })
+
+   app.listen(port, () => {
+       console.log(`Example app is listening on port http://localhost:${port}`)
+   })
