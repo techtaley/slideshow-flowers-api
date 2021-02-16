f106eddf7f231c04484fa904c254033d83474e3b
TaLoveous Goodwin <TaLoveous@expansivedesigns.com>
2021-02-15 22:03:41 -0500

    updated index

--- index.js
+++ index.js
@@ -1,28 +1,17 @@
-const server = jsonServer.create()
-const router = jsonServer.router('flowers.json')
-const middlewares = jsonServer.defaults()
-const port = process.env.port || 4001
+   const express = require("express")
+   const app = express()
+   const importData = require("./flowers.json")
+   const port = process.env.PORT || 4001 
 
-server.use(middlewares)
-server.use(router)
-server.listen(port, () => {
-    console.log(`Flowers API is listening on port http://localhost:${port}`)
-})  
+   //routes here
+   app.get("/", (req, res) => {  
+       res.send("Flower shop") 
+  })
 
-//    const express = require("express")
-//    const app = express()
-//    const importData = require("./flowers.json")
-//
-//    //routes here
-//    app.get("/", (req, res) => {  
-//        res.send("Flower shop") 
-//   })
+   app.get("/", (req, res) => {
+       res.send(importData)
+   })
 
-//    app.get("/flowers", (req, res) => {
-//        res.send(importData)
-//    })
-
-//    app.listen(port, () => {
-//        console.log(`Example app is listening on port http://localhost:${port}`)
-//    })
-
+   app.listen(port, () => {
+       console.log(`Example app is listening on port http://localhost:${port}`)
+   })
